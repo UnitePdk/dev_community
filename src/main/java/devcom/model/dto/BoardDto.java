ba5e696d497217c37a5d07f18101ee67379350a7
UnitePdk <qkrrjs1515@gmail.com>
2025-02-06 21:16:39 +0900

    0206

--- src/main/java/devcom/model/dto/BoardDto.java
+++ src/main/java/devcom/model/dto/BoardDto.java
@@ -1,4 +1,26 @@
 package devcom.model.dto;
 
+import devcom.model.entity.BoardEntity;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BoardDto {
+    private int bno;
+    private String btitle;
+    private String bcontent;
+    private int bview;
+    private int blike;
+    private int cno;
+    private int mno;
+    private String cname;
+    private String cdate;
+
+    public BoardEntity toEntity() {
+        return BoardEntity.builder().btitle(this.btitle).bcontent(this.bcontent).bview(this.bview).blike(this.blike).build();
+    }
 }
