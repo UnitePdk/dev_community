2633a349bf69a343920b5687ce8936a81dfb07fb
kmskmk21 <kmskmk21@gmail.com>
2025-02-07 00:41:10 +0900

    2025.02.06

--- src/main/java/devcom/model/dto/MessageDto.java
+++ src/main/java/devcom/model/dto/MessageDto.java
@@ -1,5 +1,6 @@
 package devcom.model.dto;
 
+import devcom.model.entity.MessageEntity;
 import lombok.*;
 
 @Getter
@@ -9,10 +10,23 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class MessageDto {
-    // 식별코드
-    private int meno; // 회원번호
+    private int meno;               // 쪽지 번호
+    private String metitle;         // 제목
+    private String mecontent;       // 내용
+    private int sendermno;          // 송신자
+    private int receivermno;        // 수신자
+    private String cdate;           // 송신 시간
 
-    // 제목
-    // 내용
-    //
+//    // 화면 표시 (회원번호x, 아이디 출력)
+//    private String sendmid;         // 송신자 아이디
+//    private String receivermid;     // 수신자 아이디
+
+//    // dto -> entity
+//    public MessageEntity toEntity() {
+//        return MessageEntity.builder()
+//                .metitle(this.metitle)
+//                .mecontent(this.mecontent)
+//                .build();
+//    }
+
 }
--- src/main/java/devcom/model/entity/MessageEntity.java
+++ src/main/java/devcom/model/entity/MessageEntity.java
@@ -4,8 +4,6 @@ import devcom.model.dto.MessageDto;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.format.DateTimeFormatter;
-
 @Getter @Setter @ToString @Builder // 롬복
 @AllArgsConstructor @NoArgsConstructor // 롬복
 @Entity
@@ -20,18 +18,27 @@ public class MessageEntity {
     private String metitle; // 제목
     @Column(nullable = false, columnDefinition = "longtext")
     private String mecontent; // 내용
-    @Column
+
+    // sender_mno와 receiver_mno는 각각 외래 키로 다른 컬럼을 참조하도록 설정
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "sender_mno", insertable = false, updatable = false)  // 송신자 외래 키
+    private MemberEntity sendermno; // 송신자
 
     @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "mno")
-    private MemberEntity sender; //송수신자
-
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "mno")
-    private MemberEntity receiver; //송수신자
+    @JoinColumn(name = "receiver_mno", insertable = false, updatable = false)  // 수신자 외래 키
+    private MemberEntity receivermno; // 수신자
 
 
-    // entity -> dto
+//    // entity -> dto
+//    public MessageDto toDto() {
+//        return MessageDto.builder()
+//                .meno(this.meno)
+//                .metitle(this.metitle)
+//                .mecontent(this.mecontent)
+//                .receivermno(this.receivermno.getMno())
+//                .sendermno(this.sendermno.getMno())
+//                .build();
+//    }
 
 
 }
