40ee08ad417add43dd17182d969e6c609c79bb5d
UnitePdk <qkrrjs1515@gmail.com>
2025-02-07 21:49:56 +0900

    0207

--- src/main/java/devcom/controller/BoardController.java
+++ src/main/java/devcom/controller/BoardController.java
@@ -1,4 +1,29 @@
 package devcom.controller;
 
+import devcom.model.dto.BoardDto;
+import devcom.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class BoardController {
+    @Autowired
+    private BoardService boardService;
+
+    // 게시물 목록 조회 - 질문
+    @GetMapping("/board/ask.do")
+    public List<BoardDto>boardAsk(){
+        return boardService.boardAsk();
+    }
+
+    // 게시물 목록 조회 - 질문
+
+    // 게시물 목록 조회 - 질문
+
+    // 게시물 목록 조회 - 질문
+
+    // 게시물 목록 조회 - 전체
 }
--- src/main/java/devcom/service/BoardService.java
+++ src/main/java/devcom/service/BoardService.java
@@ -1,4 +1,40 @@
 package devcom.service;
 
+import devcom.model.dto.BoardDto;
+import devcom.model.entity.BoardEntity;
+import devcom.model.repository.BoardRepository;
+import devcom.model.repository.CategoryRepository;
+import devcom.model.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class BoardService {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    BoardRepository boardRepository;
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    public List<BoardDto> boardAsk() {
+        List<BoardEntity> boardEntityList = boardRepository.findAll();
+
+        // 질문 카테고리만 dto로 변환
+        List<BoardDto> boardDtoList = new ArrayList<>();
+        boardEntityList.forEach(entity -> {
+            if (entity.getCategoryEntity().getCno() == 1) {
+                BoardDto boardDto = entity.toDto();
+                boardDtoList.add(boardDto);
+            }
+        });
+
+        return boardDtoList;
+    }
 }
