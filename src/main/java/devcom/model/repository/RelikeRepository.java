1835dc1d86b84919a89deedd790bae535b81d1b9
songjungah <1104wjddk@naver.com>
2025-02-10 21:24:21 +0900

    250210

--- src/main/java/devcom/model/repository/RelikeRepository.java
+++ src/main/java/devcom/model/repository/RelikeRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface RelikeRepository extends JpaRepository<ReplyEntity, Long> {
 
     // 중복 체크
-    boolean existsReplyAndMember(Long replyId, Long memberId);
+    // boolean existsReplyAndMember(Long replyId, Long memberId);
 
     // 좋아요 취소
-    void deleteReplyAndMember(Long replyId, Long memberId);
+    // void deleteReplyAndMember(Long replyId, Long memberId);
 
 }
