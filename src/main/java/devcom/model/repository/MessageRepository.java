40c4c999d9f2df28144b1390ad7e05cf08857745
kimms0123 <161434331+kimms0123@users.noreply.github.com>
2025-02-10 18:49:16 +0900

    Update MessageRepository.java

--- src/main/java/devcom/model/repository/MessageRepository.java
+++ src/main/java/devcom/model/repository/MessageRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface MessageRepository extends JpaRepository<MessageEntity, Integer> {    List<MessageEntity> findByReceiver(MemberEntity memberEntity);
+public interface MessageRepository extends JpaRepository<MessageEntity, Integer> {
+    List<MessageEntity> findByReceiver(MemberEntity memberEntity);
     List<MessageEntity> findBySender(MemberEntity memberEntity);
 
 
