3e8e7fd60326d74039a6ef1e670c424e5e809501
UnitePdk <qkrrjs1515@gmail.com>
2025-02-12 15:27:11 +0900

    reply 병합

--- src/main/java/devcom/model/repository/ReplyRepository.java
+++ src/main/java/devcom/model/repository/ReplyRepository.java
@@ -2,6 +2,8 @@ package devcom.model.repository;
 
 import devcom.model.entity.ReplyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReplyRepository extends JpaRepository<ReplyEntity, Integer> {
 }
